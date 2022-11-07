8e40c90d1ac863355b678e9243f0ac9b5d9c2e5d
Maajid Al Rasheeth <83228386+AlMaajid@users.noreply.github.com>
2022-11-07 15:15:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let numEl = document.getElementById('num');
+let saveEl = document.getElementById('save-entries');
+let count = 0;
+
+function increment() {
+  count += 1;
+  numEl.innerText = count;
+}
+
+function save() {
+  let countStr = count + "-"
+  saveEl.innerText += countStr;
+}
+
+function reset() {
+  count = 0;
+  numEl.innerText = count;
+}
